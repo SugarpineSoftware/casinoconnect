f155aa118e91290204b1e70c2b5535a12e2c4135
Cory Green <corygreen84@hotmail.com>
2019-10-06 03:00:02 -0700

    setting up a guard for authentication status

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './authentication.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule',
+    canActivate: [AuthenticationGuard] }
 ];
 @NgModule({
   imports: [
---
+++ src/app/authentication.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate  {
+  
+  constructor(private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      // here we will setup a statement for firebase to say if the user is still logged in //
+      // if they arent still logged in, then we boot them back to the main login screen //
+      const loggedIn = Math.random() > 0.5 ? true : false;
+
+      if (!loggedIn) {
+        this.router.navigate(['/']);
+      }
+
+      console.log('Boolean value for logged out is ' + loggedIn);
+      return loggedIn;
+    }
+}
