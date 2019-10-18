a6e8a2d6faba9547b5228e9f7378b495b789eddf
Sugarpine Software <cory@sugarpinesoftware.com>
2019-10-18 11:20:30 -0700

    Tab2 for code generation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,13 +5,6 @@ import { ToastController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 
-
-const Bally = {manufacture: 'Bally', type: [ 'S6000', 'S9000']};
-const IGT = {manufacture: 'IGT', type: ['S2000', 'GameKing', 'Igame', 'S3000', 'Crystal Core', 'Crystal Duo']};
-const Aristocrat = {manufacture: 'Aristocrate', type: [ 'MKVI', 'Viridian', 'Helix']};
-
-const DropDownList = [Bally, IGT, Aristocrat];
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -39,23 +32,5 @@ export class Tab1Page {
   // logging out the user //
   logoutOnClick() {
     this.auth.auth.signOut();
-  }
-
-
-  downloadQR() {
-    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-    const imageData = canvas.toDataURL('image/png').toString();
-
-    const data = imageData.split(',')[1];
-
-    this.base64ToGallery.base64ToGallery(data,
-      { prefix: '_img', mediaScanner: true})
-      .then(async res => {
-        const toast = await this.toastCtrl.create({
-          header: 'QR Code saved'
-        });
-        toast.present();
-      }, err => console.log('err: ', err)
-      );
   }
 }
