3e322c07270499dc7f9d62df0d09726a687eae15
Sugarpine Software <cory@sugarpinesoftware.com>
2019-10-19 10:39:46 -0700

    Decryption on QR Scanner

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,8 @@ import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { ToastController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as CryptoJS from 'crypto-js';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,18 +12,39 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['tab1.page.scss']
 })
 
+
 export class Tab1Page {
   qrData = 'Serial Number';
   scannedCode = null;
   elementType: 'url' | 'canvas' | 'img' = 'canvas';
   constructor(private barcodeScanner: BarcodeScanner,
-              private auth: AngularFireAuth) {
+              private auth: AngularFireAuth,
+              public alertController: AlertController) {
   }
 
+  public encrypted: string;
+  public decrypted: string;
+  private SECRET_KEY: string = "LodonGreen";
+  codeDecryption(){
+    this.decrypted = CryptoJS.AES.decrypt(this.encrypted,this.SECRET_KEY).toString(CryptoJS.enc.Utf8);
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'QR Data',
+      subHeader: this.encrypted,
+      message: this.decrypted,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
   scanCode() {
     this.barcodeScanner.scan().then(
       barcodeData => {
-        // this.scannedCode = barcodeData.text;
+         this.scannedCode = barcodeData.text;
+         this.encrypted = barcodeData.text;
+         this.codeDecryption();
+         this.presentAlert();
       }
     );
   }
