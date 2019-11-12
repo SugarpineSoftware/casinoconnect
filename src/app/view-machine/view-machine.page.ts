01ca414c69df94d908d24174e83182d67a54c09e
Cory Green <corygreen84@hotmail.com>
2019-11-11 18:54:18 -0800

    c

--- src/app/view-machine/view-machine.page.ts
+++ src/app/view-machine/view-machine.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterContentInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Data } from '@angular/router';
 import { DataPassService } from '../data-pass.service';
 
@@ -7,7 +7,7 @@ import { DataPassService } from '../data-pass.service';
   templateUrl: './view-machine.page.html',
   styleUrls: ['./view-machine.page.scss'],
 })
-export class ViewMachinePage implements OnInit, AfterContentInit {
+export class ViewMachinePage implements OnInit{
 
   constructor(
     private DataPass: DataPassService
@@ -17,13 +17,5 @@ export class ViewMachinePage implements OnInit, AfterContentInit {
 
   ngOnInit() {
     this.m = this.DataPass.getDataObject();
-    // console.log('in here yo ' + this.m[0].serial);
-    /*
-    this.m.forEach(element => {
-      console.log(element);
-    });
-    */
   }
-
-
 }
